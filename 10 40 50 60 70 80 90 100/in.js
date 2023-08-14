132c5e239283ff1900a0a8222ceeb45d13b76c3e
EXPD <shenntykus@gmail.com>
2023-08-14 17:18:35 +0300

    lesson1
    Co-Authored-By: EXPD <79328909+cripp122@users.noreply.github.com>

---
+++ 10 40 50 60 70 80 90 100/in.js
@@ -0,0 +1,11 @@
+// for (let i =10 ; i >=0; i-- ){
+//     console.log(i)
+ 
+// }
+let array = ['Krokodil', 'krokodilblack', 'krokodilwhite','krokodilgreen', 'krogodilyellow']
+
+console.log(array.lenght) //тут ошибка если че. Понедельник-среда на связи
+console.log(array[4])
+for (let index = 0; index < array.lenght; index++) {
+    console.log(array[index])   
+}
---
+++ 10 40 50 60 70 80 90 100/test.js
@@ -0,0 +1,7 @@
+let key = prompt(0)
+
+for (let index = 0; index < 1000;index++) {
+if(index+2){
+    console.log(`${index}ты ввел число меньше 1 `)
+}
+}
